3f20a659e43cd31600ed3125bbc05f77fd00f006
imperishableSecret <liferacer2310@gmail.com>
2026-05-14 01:14:58 +0530

    Restore workspace activation from selection

--- src/overview/scroll/ScrollOverviewSelection.cpp
+++ src/overview/scroll/ScrollOverviewSelection.cpp
@@ -311,9 +311,9 @@ bool CScrollOverview::moveSelection(Vector2D direction) {
 
     if (!moved && g_hyprviewConfig.keyboard.wrap && !workspaceEntries.empty()) {
         if (MOVE_DOWN && viewportCurrentWorkspace == workspaceEntries.size() - 1)
-            moved = setViewportWorkspace(0, false, false);
+            moved = setViewportWorkspace(0, false, true);
         else if (!MOVE_DOWN && viewportCurrentWorkspace == 0)
-            moved = setViewportWorkspace(workspaceEntries.size() - 1, false, false);
+            moved = setViewportWorkspace(workspaceEntries.size() - 1, false, true);
     }
 
     if (!moved)
